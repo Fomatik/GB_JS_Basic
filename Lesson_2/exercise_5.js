4479a9cb381bef28e598f57eacfe55667da44b31
Fomatik <Fomatikya@ya.ru>
2021-09-09 17:31:37 +0300

    complete lesson_2 exercises

---
+++ Lesson_2/exercise_5.js
@@ -0,0 +1,27 @@
+/* 5. Реализовать четыре основные арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return. */
+
+//сложение
+function addition(a, b) {
+    return console.log(a + b);
+}
+
+//вычитание
+function subtraction(a, b) {
+    return console.log(a - b);
+}
+
+//умножение
+function multiplication(a, b) {
+    return console.log(a * b);
+}
+
+//деление
+function division(a, b) {
+    return console.log(a / b);
+}
+
+//тест
+addition(2, 2);
+subtraction(2, 2);
+multiplication(2, 2);
+division(2, 2);
